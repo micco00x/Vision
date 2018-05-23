8950350f6a59b3b78c1241cdbb2fe2ec28ea467e
Michele Cipriano <micco00x@gmail.com>
2018-05-23 21:34:36 +0200

    common.py introduced

---
+++ common.py
@@ -0,0 +1,6 @@
+activity_classes_names = ["Crunches mat", "Aerobic Step", "Uneven bars", "Bar", "Pommel horse",
+                          "elliptical training machine", "Balance Beam", "Rowing machine", "Handle",
+                          "Rope", "Bicycle", "Parallel bars", "Bars"]
+
+COCO_NUM_CLASSES = 80
+ACTIVITY_NUM_CLASSES = len(activity_classes_names)
