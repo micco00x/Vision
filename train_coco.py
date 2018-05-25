c971c52773507fa8f7337398c3fed73d92ee3bfc
Michele Cipriano <micco00x@gmail.com>
2018-05-25 21:51:58 +0200

    Code cleaned

--- train_coco.py
+++ train_coco.py
@@ -69,10 +69,10 @@ COCO_MODEL_PATH = "weights/mask_rcnn_coco.h5"
 DEFAULT_LOGS_DIR = "logs"
 DEFAULT_DATASET_YEAR = "2014"
 
+
 ############################################################
 #  Configurations
 ############################################################
-
 
 class ExtendedCocoConfig(Config):
     """Configuration for training on MS COCO.
@@ -91,7 +91,6 @@ class ExtendedCocoConfig(Config):
 
     # Number of classes (including background)
     NUM_CLASSES = 1 + common.COCO_NUM_CLASSES + common.ACTIVITY_NUM_CLASSES
-
 
 ############################################################
 #  Dataset
@@ -313,96 +312,14 @@ class ExtendedCocoDataset(ActivityDataset):
         m = maskUtils.decode(rle)
         return m
 
-
 ############################################################
 #  COCO Evaluation
 ############################################################
 
-def build_coco_results(dataset, image_ids, rois, class_ids, scores, masks):
-    """Arrange resutls to match COCO specs in http://cocodataset.org/#format
-    """
-    # If no results, return an empty list
-    if rois is None:
-        return []
-
-    results = []
-    for image_id in image_ids:
-        # Loop through detections
-        for i in range(rois.shape[0]):
-            class_id = class_ids[i]
-            score = scores[i]
-            bbox = np.around(rois[i], 1)
-            mask = masks[:, :, i]
-
-            result = {
-                "image_id": image_id,
-                "category_id": dataset.get_source_class_id(class_id, "coco"),
-                "bbox": [bbox[1], bbox[0], bbox[3] - bbox[1], bbox[2] - bbox[0]],
-                "score": score,
-                "segmentation": maskUtils.encode(np.asfortranarray(mask))
-            }
-            results.append(result)
-    return results
-
-
-def evaluate_coco(model, dataset, coco, eval_type="bbox", limit=0, image_ids=None):
-    """Runs official COCO evaluation.
-    dataset: A Dataset object with valiadtion data
-    eval_type: "bbox" or "segm" for bounding box or segmentation evaluation
-    limit: if not 0, it's the number of images to use for evaluation
-    """
-    # Pick COCO images from the dataset
-    image_ids = image_ids or dataset.image_ids
-
-    # Limit to a subset
-    if limit:
-        image_ids = image_ids[:limit]
-
-    # Get corresponding COCO image IDs.
-    coco_image_ids = [dataset.image_info[id]["id"] for id in image_ids]
-
-    t_prediction = 0
-    t_start = time.time()
-
-    results = []
-    for i, image_id in enumerate(image_ids):
-        # Load image
-        image = dataset.load_image(image_id)
-
-        # Run detection
-        t = time.time()
-        r = model.detect([image], verbose=0)[0]
-        t_prediction += (time.time() - t)
-
-        # Convert results to COCO format
-        # Cast masks to uint8 because COCO tools errors out on bool
-        image_results = build_coco_results(dataset, coco_image_ids[i:i + 1],
-                                           r["rois"], r["class_ids"],
-                                           r["scores"],
-                                           r["masks"].astype(np.uint8))
-        results.extend(image_results)
-
-    # Load results. This modifies results with additional attributes.
-    coco_results = coco.loadRes(results)
-
-    # Evaluate
-    cocoEval = COCOeval(coco, coco_results, eval_type)
-    cocoEval.params.imgIds = coco_image_ids
-    cocoEval.evaluate()
-    cocoEval.accumulate()
-    cocoEval.summarize()
-
-    print("Prediction time: {}. Average {}/image".format(
-        t_prediction, t_prediction / len(image_ids)))
-    print("Total time: ", time.time() - t_start)
-
-
 def evaluate_extended_coco(model, dataset, config):
     # Compute VOC-Style mAP @ IoU=0.5
-    # Running on 10 images. Increase for better accuracy.
-    image_ids = np.random.choice(dataset.image_ids, 10)
     APs = []
-    for image_id in image_ids:
+    for image_id in dataset.image_ids:
         # Load image and ground truth data
         image, image_meta, gt_class_id, gt_bbox, gt_mask = modellib.load_image_gt(dataset, config, image_id, use_mini_mask=False)
         molded_images = np.expand_dims(modellib.mold_image(image, config), 0)
@@ -419,7 +336,6 @@ def evaluate_extended_coco(model, dataset, config):
 ############################################################
 #  Training
 ############################################################
-
 
 if __name__ == '__main__':
     import argparse
@@ -476,11 +392,9 @@ if __name__ == '__main__':
 
     # Create model
     if args.command == "train":
-        model = modellib.MaskRCNN(mode="training", config=config,
-                                  model_dir=args.logs)
+        model = modellib.MaskRCNN(mode="training", config=config, model_dir=args.logs)
     else:
-        model = modellib.MaskRCNN(mode="inference", config=config,
-                                  model_dir=args.logs)
+        model = modellib.MaskRCNN(mode="inference", config=config, model_dir=args.logs)
 
     # Select weights file to load
     if args.model.lower() == "coco":
@@ -558,8 +472,6 @@ if __name__ == '__main__':
         dataset_val.load_activity("dataset/trainval/val.json")
         dataset_val.prepare()
         print("Running COCO evaluation on {} images.".format(args.limit))
-        #evaluate_coco(model, dataset_val, coco, "bbox", limit=int(args.limit))
-        #evaluate_coco(model, dataset, coco, eval_type="bbox", image_ids=dataset_val._image_ids)
         evaluate_extended_coco(model, dataset_val, config)
     else:
         print("'{}' is not recognized. "
