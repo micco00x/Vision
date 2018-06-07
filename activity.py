7fbb605bf5813b342d45864f28d3865a8d2326a8
Michele Cipriano <micco00x@gmail.com>
2018-06-07 11:09:27 +0200

    Evaluation now handles exceptions

--- activity.py
+++ activity.py
@@ -395,16 +395,19 @@ def evaluate(model, dataset, config):
     APs = []
     cnt = 0
     for image_id in dataset.image_ids:
-        # Load image and ground truth data
-        image, image_meta, gt_class_id, gt_bbox, gt_mask = modellib.load_image_gt(dataset, config, image_id, use_mini_mask=False)
-        molded_images = np.expand_dims(modellib.mold_image(image, config), 0)
-        # Run object detection
-        results = model.detect([image], verbose=0)
-        r = results[0]
-        # Compute AP
-        AP, precisions, recalls, overlaps = utils.compute_ap(gt_bbox, gt_class_id, gt_mask,
-                                                             r["rois"], r["class_ids"], r["scores"], r['masks'])
-        APs.append(AP)
+        try:
+            # Load image and ground truth data
+            image, image_meta, gt_class_id, gt_bbox, gt_mask = modellib.load_image_gt(dataset, config, image_id, use_mini_mask=False)
+            molded_images = np.expand_dims(modellib.mold_image(image, config), 0)
+            # Run object detection
+            results = model.detect([image], verbose=0)
+            r = results[0]
+            # Compute AP
+            AP, precisions, recalls, overlaps = utils.compute_ap(gt_bbox, gt_class_id, gt_mask,
+                                                                 r["rois"], r["class_ids"], r["scores"], r['masks'])
+            APs.append(AP)
+        except:
+            print("Error while doing inference on image with image_id=" + str(image_id))
 
         # Printing the progress while evaluating the model:
         cnt = cnt + 1
