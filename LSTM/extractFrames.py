ff75a03e02bc34994f2c1aca987caa1e45116117
Michele Cipriano <micco00x@gmail.com>
2018-06-26 22:22:11 +0200

    extractFrames.py now supporting argparse

--- LSTM/extractFrames.py
+++ LSTM/extractFrames.py
@@ -2,11 +2,20 @@ import os
 import glob
 import cv2
 import shutil
+import argparse
 
-numberFrames = 40
-trim = 0.2
-videoFolder = "Videos"
-framesFolder = "Frames"
+# Parse arguments from command line:
+parser = argparse.ArgumentParser(description="Extract frames from a set of videos and create a dataset")
+parser.add_argument("--numframes", required=False, default=40, help="Number of frames to extract from each video (default=40)")
+parser.add_argument("--trim", required=False, default=0.2, help="How much of the video to trim (default=0.2)")
+parser.add_argument("--videofolder", required=False, default="Videos", help="Folder containing the videos (default=Videos)")
+parser.add_argument("--framesfolder", required=False, default="Frames", help="Folder that will contain frames (default=Frames)")
+args = parser.parse_args()
+
+numberFrames = args.numframes
+trim = args.trim
+videoFolder = args.videofolder
+framesFolder = args.framesfolder
 
 # list of activities
 folders = os.listdir(videoFolder)
@@ -24,13 +33,15 @@ for f in folders:
     if not os.path.exists(framesFolder + "/" + f):
         os.makedirs(framesFolder + "/" + f)
 
-    for video in files:
+    for idx, video in enumerate(files):
+
+        print("Reading {}: {:2.1%}".format(f, idx / len(files)), end="\r")
 
         # create video folder
         _, videoName = os.path.split(os.path.splitext(video)[0])
         if not os.path.exists(framesFolder + "/" + f + "/" + videoName):
             os.makedirs(framesFolder + "/" + f + "/" + videoName)
-        
+
         # open video
         vidcap = cv2.VideoCapture(video)
 
@@ -47,3 +58,5 @@ for f in folders:
                 print("Error")
 
             cv2.imwrite(framesFolder + "/" + f + "/" + videoName + "/%d.jpg" % i, image)  # save frame as JPEG file
+
+    print("Reading {}: COMPLETED".format(f))
