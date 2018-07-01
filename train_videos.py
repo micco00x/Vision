5feca303d453a317b6b4b3a3da6a4181fb3ca84a
Michele Cipriano <micco00x@gmail.com>
2018-07-02 01:05:39 +0200

    LSTM training now supports validation set

--- train_videos.py
+++ train_videos.py
@@ -115,9 +115,12 @@ def generate_tensors(dataset_filename, model_dir):
 
 # Parse arguments from command line:
 parser = argparse.ArgumentParser(description="Train a network to classify videos in activities")
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument("--dataset", help=".txt file with paths to videos' frames")
-group.add_argument("--npz", help=".npz file that stores tensors used to train the network")
+train_group = parser.add_mutually_exclusive_group(required=True)
+train_group.add_argument("--train_dataset", help="Train .txt file with paths to videos' frames")
+train_group.add_argument("--train_npz", help="Train .npz file that stores tensors used to train the network")
+val_group = parser.add_mutually_exclusive_group(required=True)
+val_group.add_argument("--val_dataset", help="Validation .txt file with paths to videos' frames")
+val_group.add_argument("--val_npz", help="Validation .npz file that stores tensors used to train the network")
 parser.add_argument("--logs", required=False,
                     default=DEFAULT_LOGS_DIR,
                     metavar="/path/to/logs/",
@@ -131,48 +134,65 @@ parser.add_argument("--epochs", default=10, type=int, help="Number of epochs to 
 parser.add_argument("--lstm_hidden", default=256, type=int, help="Size of the hidden layer for the LSTM")
 args = parser.parse_args()
 
-# Load tensors or generate them depending on the arguments:
-if args.npz:
-    npzfile = np.load(args.npz)
-    videos = npzfile["videos"]
-    videos_y = npzfile["videos_y"]
+# Load tensors or generate them depending on the arguments (training):
+if args.train_npz:
+    npzfile = np.load(args.train_npz)
+    train_videos = npzfile["videos"]
+    train_videos_y = npzfile["videos_y"]
 else:
-    videos, videos_y = generate_tensors(args.dataset, args.logs)
+    train_videos, train_videos_y = generate_tensors(args.train_dataset, args.logs)
 
     # Save tensors in .npz format (in the same path of the dataset .txt)
     # to speed up loading if doing multiple trainings:
-    if args.npz == None:
-        idx_extension = args.dataset.rfind(".")
-        np.savez(args.dataset[:idx_extension] + ".npz", videos=videos, videos_y=videos_y)
+    if args.train_npz == None:
+        idx_extension = args.train_dataset.rfind(".")
+        np.savez(args.train_dataset[:idx_extension] + ".npz", videos=train_videos, videos_y=train_videos_y)
+
+# Load tensors or generate them depending on the arguments (validation):
+if args.val_npz:
+    npzfile = np.load(args.val_npz)
+    val_videos = npzfile["videos"]
+    val_videos_y = npzfile["videos_y"]
+else:
+    val_videos, val_videos_y = generate_tensors(args.val_dataset, args.logs)
+
+    # Save tensors in .npz format (in the same path of the dataset .txt)
+    # to speed up loading if doing multiple trainings:
+    if args.val_npz == None:
+        idx_extension = args.val_dataset.rfind(".")
+        np.savez(args.val_dataset[:idx_extension] + ".npz", videos=val_videos, videos_y=val_videos_y)
 
 # videos should be [num videos, num frames, width, height, channels],
 # videos_y should be [num_videos, ]:
-print(videos.shape)
-print(videos_y.shape)
+print(train_videos.shape)
+print(train_videos_y.shape)
+print(val_videos.shape)
+print(val_videos_y.shape)
 
 # Convert videos' frames from uint8 to float32, specifically from [0,255] to [0,1]:
-videos = videos.astype(np.float32) / 255
+train_videos = train_videos.astype(np.float32) / 255
+val_videos = val_videos.astype(np.float32) / 255
 #print("Checking videos are in range [0,1]:", (0 <= videos).all() and (videos <= 1).all())
 
 # LSTM hyperparameters:
 lstm_hidden = args.lstm_hidden
-num_classes = len(set(videos_y))
+num_classes = len(set(train_videos_y))
 learning_rate = args.learning_rate
 epochs = args.epochs
 
 # Train the LSTM on the dataset:
 with tf.Session() as sess:
     # Input (videos' frames) and output (indices) of the network:
-    X = tf.placeholder(tf.float32, shape=[None] + list(videos.shape[1:]))
-    y = tf.placeholder(tf.uint8, shape=[None] + list(videos_y.shape[1:]))
+    X = tf.placeholder(tf.float32, shape=[None] + list(train_videos.shape[1:]))
+    y = tf.placeholder(tf.uint8, shape=[None] + list(train_videos_y.shape[1:]))
     batch_size = tf.shape(X)[0]
 
     # Reshape and unstack the input so that it can be fed to the LSTM:
     # Reshape X to [batch_size, num frames, width*height*channels] and then
     # unstack it to a list of length num frames containing tensors of
     # shape [batch_size, width*height*channels]:
-    X_reshaped  = tf.reshape(X, [batch_size, videos.shape[1], np.prod(videos.shape[2:])])
-    X_unstacked = tf.unstack(X_reshaped, videos.shape[1], axis=1)
+    X_reshaped  = tf.reshape(X, [batch_size, train_videos.shape[1], np.prod(train_videos.shape[2:])])
+    X_unstacked = tf.unstack(X_reshaped, train_videos.shape[1], axis=1)
 
     # LSTM part:
     rnn_cell = tf.contrib.rnn.BasicLSTMCell(lstm_hidden)
@@ -200,10 +220,9 @@ with tf.Session() as sess:
     tf.global_variables_initializer().run()
 
     # Train the network:
-    step = 0
     for epoch in range(epochs):
         print("Epoch {}/{}:".format(epoch+1, epochs))
-        new_indices = np.random.randint(videos.shape[0], size=videos.shape[0])
+        new_indices = np.random.randint(train_videos.shape[0], size=train_videos.shape[0])
 
         # Let mu_i = vloss for each iteration, let's say the iteration is N,
         # bs is the batch_size of the current iteration,
@@ -218,13 +237,13 @@ with tf.Session() as sess:
         sum_accuracy = 0
         tot_accuracy = 0
 
-        # Iterate through the dataset using new_indices (random shuffle):
-        for idx in range(0, videos.shape[0], args.batch_size):
+        # Iterate through the train dataset using new_indices (random shuffle):
+        for idx in range(0, train_videos.shape[0], args.batch_size):
             # Extract the following args.batch_size indices and perform a training step:
-            L = min(idx+args.batch_size, videos.shape[0])
+            L = min(idx+args.batch_size, train_videos.shape[0])
             rand_idx = new_indices[idx:L]
-            minibatch_X = videos[rand_idx,:,:,:,:]
-            minibatch_y = videos_y[rand_idx]
+            minibatch_X = train_videos[rand_idx,:,:,:,:]
+            minibatch_y = train_videos_y[rand_idx]
             vloss, vaccuracy, bs, _ = sess.run([loss, accuracy, batch_size, train_step], feed_dict={X: minibatch_X, y: minibatch_y})
 
             # Update tot_loss and tot_accuracy:
@@ -234,14 +253,44 @@ with tf.Session() as sess:
             tot_accuracy = (args.batch_size * sum_accuracy + bs * vaccuracy) / (N * args.batch_size + bs)
             sum_accuracy = sum_accuracy + vaccuracy
 
-            # Update tot_loss and tot_accuracy and log to TensorBoard:
-            # TODO: move this inside the graph.
-            step = step + 1
-            summary = tf.Summary(value=[tf.Summary.Value(tag="loss", simple_value=tot_loss)])
-            train_writer.add_summary(summary, step)
-            summary = tf.Summary(value=[tf.Summary.Value(tag="accuracy", simple_value=tot_accuracy)])
-            train_writer.add_summary(summary, step)
+            print("Progress: {}/{} - train_loss: {:2.3} - train_accuracy: {:2.3}".format(L, train_videos.shape[0], tot_loss, tot_accuracy), end="\r")
+        print("Progress: {}/{} - train_loss: {:2.3} - train_accuracy: {:2.3}".format(train_videos.shape[0], train_videos.shape[0], tot_loss, tot_accuracy))
 
-            print("Progress: {}/{} - Loss: {:2.3} - Accuracy: {:2.3}".format(L, videos.shape[0], tot_loss, tot_accuracy), end="\r")
-            sess.run(train_step, feed_dict={X: minibatch_X, y: minibatch_y})
-        print("Progress: {}/{} - Loss: {:2.3} - Accuracy: {:2.3}".format(videos.shape[0], videos.shape[0], tot_loss, tot_accuracy))
+        # Update tot_loss and tot_accuracy and log to TensorBoard:
+        # TODO: move this inside the graph.
+        summary = tf.Summary(value=[tf.Summary.Value(tag="train_loss", simple_value=tot_loss)])
+        train_writer.add_summary(summary, epoch)
+        summary = tf.Summary(value=[tf.Summary.Value(tag="train_accuracy", simple_value=tot_accuracy)])
+        train_writer.add_summary(summary, epoch)
+
+        # TODO: this second part is identical to the first one except for train_step.
+        N = 0
+        sum_loss = 0
+        tot_loss = 0
+        sum_accuracy = 0
+        tot_accuracy = 0
+
+        # Iterate through the val dataset using new_indices (random shuffle):
+        for idx in range(0, val_videos.shape[0], args.batch_size):
+            # Extract the following args.batch_size indices and perform a training step:
+            L = min(idx+args.batch_size, val_videos.shape[0])
+            minibatch_X = val_videos[idx:L,:,:,:,:]
+            minibatch_y = val_videos_y[idx:L]
+            vloss, vaccuracy, bs = sess.run([loss, accuracy, batch_size], feed_dict={X: minibatch_X, y: minibatch_y})
+
+            # Update tot_loss and tot_accuracy:
+            N = N + 1
+            tot_loss = (args.batch_size * sum_loss + bs * vloss) / (N * args.batch_size + bs)
+            sum_loss = sum_loss + vloss
+            tot_accuracy = (args.batch_size * sum_accuracy + bs * vaccuracy) / (N * args.batch_size + bs)
+            sum_accuracy = sum_accuracy + vaccuracy
+
+            print("Progress: {}/{} - val_loss: {:2.3} - val_accuracy: {:2.3}".format(L, val_videos.shape[0], tot_loss, tot_accuracy), end="\r")
+        print("Progress: {}/{} - val_loss: {:2.3} - val_accuracy: {:2.3}".format(val_videos.shape[0], val_videos.shape[0], tot_loss, tot_accuracy))
+
+        # Update tot_loss and tot_accuracy and log to TensorBoard:
+        # TODO: move this inside the graph.
+        summary = tf.Summary(value=[tf.Summary.Value(tag="val_loss", simple_value=tot_loss)])
+        train_writer.add_summary(summary, epoch)
+        summary = tf.Summary(value=[tf.Summary.Value(tag="val_accuracy", simple_value=tot_accuracy)])
+        train_writer.add_summary(summary, epoch)
