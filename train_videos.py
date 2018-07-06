27d6b62e4dce11cfb3c8a2ef925e965de818199f
Michele Cipriano <micco00x@gmail.com>
2018-07-06 12:19:16 +0200

    train_videos generating confusion matrix

--- train_videos.py
+++ train_videos.py
@@ -6,7 +6,56 @@ import time
 
 import VideoClassifier
 
+import matplotlib.pyplot as plt
+import itertools
+import sklearn
+
 DEFAULT_LOGS_DIR = "logs"
+CLASS_NAMES = ["Doing crunches", "Doing step aerobics", "Elliptical trainer",
+               "Kneeling", "Rope skipping", "Running a marathon", "Spinning",
+               "Tumbling", "Using parallel bars","Using the balance beam", "Using the pommel horse",
+               "Using the rowing machine", "Using uneven bars", "Zumba"]
+
+# From: http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+
+    np.set_printoptions(precision=2)
+    plt.figure(figsize=(7.5, 7), dpi=480, tight_layout=True)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    #plt.show()
+    plt.savefig("images/" + title)
 
 if __name__ == "__main__":
     # Parse arguments from command line:
@@ -55,10 +104,21 @@ if __name__ == "__main__":
     epochs = args.epochs
 
     with tf.Session() as sess:
+        T = time.strftime("%Y%m%dT%H%M")
         videoClassifier = VideoClassifier.VideoClassifier(lstm_hidden, num_classes,
                                                           [None] + list(train_videos.shape[1:]),
                                                           [None] + list(train_classes.shape[1:]),
-                                                          "logs/lstm" + time.strftime("%Y%m%dT%H%M"), sess)
+                                                          "logs/lstm" + T, sess)
         videoClassifier.train(train_videos, train_classes, test_videos, test_classes,
                               tf.train.RMSPropOptimizer(learning_rate=args.learning_rate),
                               args.batch_size, args.epochs, sess)
+
+        print("Generating the confusion matrix...")
+        predictions = []
+        for idx in range(0, test_videos.shape[0], args.batch_size):
+            # Extract the following batch_size indices:
+            L = min(idx+args.batch_size, test_videos.shape[0])
+            predictions.extend(np.argmax(videoClassifier.predict_batch(test_videos[idx:L], sess), axis=1))
+
+        cnf_matrix = sklearn.metrics.confusion_matrix(test_classes, predictions)
+        plot_confusion_matrix(cnf_matrix, classes=range(1, len(CLASS_NAMES)+1), title=T)
